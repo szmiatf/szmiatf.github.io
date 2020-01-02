49f3765b1c24cbb9de81ad8ab5e45fa1e6e5b72f
Mihaly Szepesi <Mihaly_Szepesi@epam.com>
2020-01-02 14:03:31 +0100

    add merkbau-override wordpress plugin

---
+++ merkbau-override/merkbau-override.php
@@ -0,0 +1,162 @@
+<?php
+/*
+Plugin Name: Merkbau csoport functions override
+Plugin URI: https://szmiatf.eu/merkbau-override/ 
+Description: Meta, Feed, RestAPI, Emoji és oEmbed tiltása
+Ver: 1.2
+Author URI: https://merkbau.hu
+*/
+/* Start Adding Functions Below this Line */
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+
+
+add_filter('json_enabled', '__return_false');
+add_filter('json_jsonp_enabled', '__return_false');
+add_filter( 'rest_api_init', 'rest_only_for_authorized_users', 99 );
+
+function rest_only_for_authorized_users($wp_rest_server){
+    if ( !is_user_logged_in() ) {
+        wp_die('sorry you are not allowed to access this data',403);
+    }
+}
+
+function itsme_disable_feed() {
+ wp_die( __( 'No feed available, please visit the <a href="'. esc_url( home_url( '/' ) ) .'">homepage</a>!' ) );
+}
+
+add_action('do_feed', 'itsme_disable_feed', 1);
+add_action('do_feed_rdf', 'itsme_disable_feed', 1);
+add_action('do_feed_rss', 'itsme_disable_feed', 1);
+add_action('do_feed_rss2', 'itsme_disable_feed', 1);
+add_action('do_feed_atom', 'itsme_disable_feed', 1);
+add_action('do_feed_rss2_comments', 'itsme_disable_feed', 1);
+add_action('do_feed_atom_comments', 'itsme_disable_feed', 1);
+remove_action( 'wp_head', 'feed_links_extra', 3 );
+remove_action( 'wp_head', 'feed_links', 2 );
+
+
+function remove_query_strings() {
+   if(!is_admin()) {
+       add_filter('script_loader_src', 'remove_query_strings_split', 15);
+       add_filter('style_loader_src', 'remove_query_strings_split', 15);
+   }
+}
+
+function remove_query_strings_split($src){
+   $output = preg_split("/(&ver|\?ver)/", $src);
+   return $output[0];
+}
+add_action('init', 'remove_query_strings');
+
+
+
+// remove_action('wp_head', 'wp_generator');
+
+
+function disable_embeds_code_init() {
+
+ // Remove the REST API endpoint.
+ remove_action( 'rest_api_init', 'wp_oembed_register_route' );
+
+ // Turn off oEmbed auto discovery.
+ add_filter( 'embed_oembed_discover', '__return_false' );
+
+ // Don't filter oEmbed results.
+ remove_filter( 'oembed_dataparse', 'wp_filter_oembed_result', 10 );
+
+ // Remove oEmbed discovery links.
+ remove_action( 'wp_head', 'wp_oembed_add_discovery_links' );
+
+ // Remove oEmbed-specific JavaScript from the front-end and back-end.
+ remove_action( 'wp_head', 'wp_oembed_add_host_js' );
+ add_filter( 'tiny_mce_plugins', 'disable_embeds_tiny_mce_plugin' );
+
+ // Remove all embeds rewrite rules.
+ add_filter( 'rewrite_rules_array', 'disable_embeds_rewrites' );
+
+ // Remove filter of the oEmbed result before any HTTP requests are made.
+ remove_filter( 'pre_oembed_result', 'wp_filter_pre_oembed_result', 10 );
+}
+
+add_action( 'init', 'disable_embeds_code_init', 9999 );
+
+function disable_embeds_tiny_mce_plugin($plugins) {
+    return array_diff($plugins, array('wpembed'));
+}
+
+function disable_embeds_rewrites($rules) {
+    foreach($rules as $rule => $rewrite) {
+        if(false !== strpos($rewrite, 'embed=true')) {
+            unset($rules[$rule]);
+        }
+    }
+    return $rules;
+}
+
+/**
+ * Disable the emoji's
+ */
+function disable_emojis() {
+ remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+ remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+ remove_action( 'wp_print_styles', 'print_emoji_styles' );
+ remove_action( 'admin_print_styles', 'print_emoji_styles' ); 
+ remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+ remove_filter( 'comment_text_rss', 'wp_staticize_emoji' ); 
+ remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+ add_filter( 'tiny_mce_plugins', 'disable_emojis_tinymce' );
+ add_filter( 'wp_resource_hints', 'disable_emojis_remove_dns_prefetch', 10, 2 );
+}
+add_action( 'init', 'disable_emojis' );
+
+/**
+ * Filter function used to remove the tinymce emoji plugin.
+ * 
+ * @param array $plugins 
+ * @return array Difference betwen the two arrays
+ */
+function disable_emojis_tinymce( $plugins ) {
+ if ( is_array( $plugins ) ) {
+ return array_diff( $plugins, array( 'wpemoji' ) );
+ } else {
+ return array();
+ }
+}
+
+/**
+ * Remove emoji CDN hostname from DNS prefetching hints.
+ *
+ * @param array $urls URLs to print for resource hints.
+ * @param string $relation_type The relation type the URLs are printed for.
+ * @return array Difference betwen the two arrays.
+ */
+function disable_emojis_remove_dns_prefetch( $urls, $relation_type ) {
+ if ( 'dns-prefetch' == $relation_type ) {
+ /** This filter is documented in wp-includes/formatting.php */
+ $emoji_svg_url = apply_filters( 'emoji_svg_url', 'https://s.w.org/images/core/emoji/2/svg/' );
+
+$urls = array_diff( $urls, array( $emoji_svg_url ) );
+ }
+
+return $urls;
+}
+
+//Remove JQuery migrate
+function remove_jquery_migrate($scripts)
+{
+    if (!is_admin() && isset($scripts->registered['jquery'])) {
+        $script = $scripts->registered['jquery'];
+        
+        if ($script->deps) { // Check whether the script has any dependencies
+            $script->deps = array_diff($script->deps, array(
+                'jquery-migrate'
+            ));
+        }
+    }
+}
+
+add_action('wp_default_scripts', 'remove_jquery_migrate');
+
+/* Stop Adding Functions Below this Line */
+
+?>
