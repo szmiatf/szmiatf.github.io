db618309bc5f97568eaff04a1cdbf37e2bd949cb
Mihaly Szepesi <Mihaly_Szepesi@epam.com>
2020-01-02 14:09:56 +0100

    increment versions in plugins files

--- merkbau-override/merkbau-group-snippets.php
+++ merkbau-override/merkbau-group-snippets.php
@@ -1,4 +1,11 @@
 <?php
+/*
+Plugin Name: Merkbau csoport functions override
+Plugin URI: https://szmiatf.eu/merkbau-override/ 
+Description: Meta, Feed, RestAPI, Emoji és oEmbed tiltása
+Ver: 1.2
+Author URI: https://merkbau.hu
+*/
 defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 
 ?>
